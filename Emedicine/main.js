0fa7a6becf4937b3fcde07950de8a1e47b96353a
MozahidHridoy <57190423+MozahidHridoy@users.noreply.github.com>
2019-12-23 13:52:28 +0600

    Add files via upload

---
+++ Emedicine/main.js
@@ -0,0 +1,210 @@
+$(document).ready(function(){
+	cat();
+	brand();
+	product();
+	//cat() is a funtion fetching category record from database whenever page is load
+	function cat(){
+		$.ajax({
+			url	:	"action.php",
+			method:	"POST",
+			data	:	{category:1},
+			success	:	function(data){
+				$("#get_category").html(data);
+				
+			}
+		})
+	}
+	//brand() is a funtion fetching brand record from database whenever page is load
+	function brand(){
+		$.ajax({
+			url	:	"action.php",
+			method:	"POST",
+			data	:	{brand:1},
+			success	:	function(data){
+				$("#get_brand").html(data);
+			}
+		})
+	}
+	//product() is a funtion fetching product record from database whenever page is load
+		function product(){
+		$.ajax({
+			url	:	"action.php",
+			method:	"POST",
+			data	:	{getProduct:1},
+			success	:	function(data){
+				$("#get_product").html(data);
+			}
+		})
+	}
+	/*	when page is load successfully then there is a list of categories when user click on category we will get category id and 
+		according to id we will show products
+	*/
+	$("body").delegate(".category","click",function(event){
+		$("#get_product").html("<h3>Loading...</h3>");
+		event.preventDefault();
+		var cid = $(this).attr('cid');
+		
+			$.ajax({
+			url		:	"action.php",
+			method	:	"POST",
+			data	:	{get_seleted_Category:1,cat_id:cid},
+			success	:	function(data){
+				$("#get_product").html(data);
+				if($("body").width() < 480){
+					$("body").scrollTop(683);
+				}
+			}
+		})
+	
+	})
+
+	/*	when page is load successfully then there is a list of brands when user click on brand we will get brand id and 
+		according to brand id we will show products
+	*/
+	$("body").delegate(".selectBrand","click",function(event){
+		event.preventDefault();
+		$("#get_product").html("<h3>Loading...</h3>");
+		var bid = $(this).attr('bid');
+		
+			$.ajax({
+			url		:	"action.php",
+			method	:	"POST",
+			data	:	{selectBrand:1,brand_id:bid},
+			success	:	function(data){
+				$("#get_product").html(data);
+				if($("body").width() < 480){
+					$("body").scrollTop(683);
+				}
+			}
+		})
+	
+	})
+	/*
+		At the top of page there is a search box with search button when user put name of product then we will take the user 
+		given string and with the help of sql query we will match user given string to our database keywords column then matched product 
+		we will show 
+	*/
+	$("#search_btn").click(function(){
+		$("#get_product").html("<h3>Loading...</h3>");
+		var keyword = $("#search").val();
+		if(keyword != ""){
+			$.ajax({
+			url		:	"action.php",
+			method	:	"POST",
+			data	:	{search:1,keyword:keyword},
+			success	:	function(data){ 
+				$("#get_product").html(data);
+				if($("body").width() < 480){
+					$("body").scrollTop(683);
+				}
+			}
+		})
+		}
+	})
+	//end
+
+
+	/*
+		Here #login is login form id and this form is available in index.php page
+		from here input data is sent to login.php page
+		if you get login_success string from login.php page means user is logged in successfully and window.location is 
+		used to redirect user from home page to profile.php page
+	*/
+	$("#login").on("submit",function(event){
+		event.preventDefault();
+		$(".overlay").show();
+		$.ajax({
+			url	:	"login.php",
+			method:	"POST",
+			data	:$("#login").serialize(),
+			success	:function(data){
+				if(data == "login_success"){
+					window.location.href = "profile.php";
+				}else if(data == "cart_login"){
+					window.location.href = "cart.php";
+				}else{
+					$("#e_msg").html(data);
+					$(".overlay").hide();
+				}
+			}
+		})
+	})
+	//end
+
+	//Get User Information before checkout
+	$("#signup_form").on("submit",function(event){
+		event.preventDefault();
+		$(".overlay").show();
+		$.ajax({
+			url : "register.php",
+			method : "POST",
+			data : $("#signup_form").serialize(),
+			success : function(data){
+				$(".overlay").hide();
+				if (data == "register_success") {
+					window.location.href = "cart.php";
+				}else{
+					$("#signup_msg").html(data);
+				}
+				
+			}
+		})
+	})
+	//Get User Information before checkout end here
+
+	//Add Product into Cart
+	$("body").delegate("#product","click",function(event){
+		var pid = $(this).attr("pid");
+		event.preventDefault();
+		$(".overlay").show();
+		$.ajax({
+			url : "action.php",
+			method : "POST",
+			data : {addToCart:1,proId:pid},
+			success : function(data){
+				count_item();
+				getCartItem();
+				$('#product_msg').html(data);
+				$('.overlay').hide();
+			}
+		})
+	})
+	//Add Product into Cart End Here
+	//Count user cart items funtion
+	count_item();
+	function count_item(){
+		$.ajax({
+			url : "action.php",
+			method : "POST",
+			data : {count_item:1},
+			success : function(data){
+				$(".badge").html(data);
+			}
+		})
+	}
+	
+	
+
+	page();
+	
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
